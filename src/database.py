0a06f0b8e504c7645c741236edd6caa65a5cca0b
Jean Calistro <jeeanrafael456@gmail.com>
2024-10-02 22:27:02 -0300

    add docker compose and env variables

--- src/database.py
+++ src/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@db/crud"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+
+SQLALCHEMY_DATABASE_URL = f"{DB_URL}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
